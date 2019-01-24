fa55c03f24fa9613ed903838b46ebcfbbf1652b9
sivanWu0222 <yirufeng@foxmail.com>
2019-01-24 08:29:11 +0800

    add source files

---
+++ Python编程快速上手-让繁琐工程自动化/Chapter 2/8导入模块/Test.py
@@ -0,0 +1,18 @@
+
+# 导入模块
+
+# 只有一个参数的range()
+for i in range(12):
+    print(i, end='\t')
+print()
+
+# 两个参数的range()
+for i in range(0, 12):
+    print(i, end='\t')
+print()
+
+# 三个参数的range()
+for i in range(0, 12, 2):
+    print(i, end='\t')
+print()
+
